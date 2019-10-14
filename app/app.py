c59114d85ffa769d5cd30b71540bac446686ebfd
Yacine Smaoui <ysmaoui@gmail.com>
2019-10-14 12:14:40 +0200

    initial commit

---
+++ app/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from flask import Flask, render_template
+from flask_restful import Resource, Api
+
+# from praytime_raspberrypi import myPrayTimes_experiments as praytimes
+import get_prayerTimes as prayertimes
+
+import json
+
+import datetime
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def home():
+    date = datetime.date.today()
+    weekday = date.strftime("%A   %Y-%m-%d")
+    prayertimes_today = (weekday, prayertimes.getTimes(date))
+
+    prayertimes_next_7_days = []
+
+    for i in range(1, 8):
+        date = datetime.date.today() + datetime.timedelta(days=i)
+        weekday = date.strftime("%A   %Y-%m-%d")
+        prayertimes_dict = (weekday, prayertimes.getTimes(date))
+        prayertimes_next_7_days.append(prayertimes_dict)
+
+    return render_template('prayers.html',
+                           times_today=prayertimes_today,
+                           times_next_days=prayertimes_next_7_days)
+
+    # return json.dumps(prayertimes.getTimes())
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
---
+++ app/get_prayerTimes.py
@@ -0,0 +1,32 @@
+from praytimes import PrayTimes
+#from datetime import date
+import datetime
+
+places = {'berlin': (52.511, 13.44013)}
+
+
+def getTimes(for_date):
+    prayTimes = PrayTimes()
+    prayTimes.setMethod('test')
+    new_settings = {
+        "imsak": '10 min',
+        "dhuhr": '0 min',
+        "asr": 'Standard',
+        "highLats": 'AngleBased',
+        "maghrib": '-6 min'
+    }
+
+    offset = {
+        "fajr": -2,
+        "dhuhr": +5,
+        "asr": +5,
+        "maghrib": 0,
+        "isha": -3
+    }
+
+    prayTimes.adjust(new_settings)
+    prayTimes.tune(offset)
+    isSummerTime = 1
+    times = prayTimes.getTimes(for_date, places['berlin'], 1, dst=isSummerTime)
+
+    return times
