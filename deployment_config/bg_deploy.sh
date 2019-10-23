74370fba2c5593b1a66142f137574de907d18bc3
Yacine Smaoui <yacine.smaoui@carmeq.com>
2019-10-23 14:04:12 +0200

    enhancing logging

--- deployment_config/bg_deploy.sh
+++ deployment_config/bg_deploy.sh
@@ -12,7 +12,18 @@ main(){
         export TARGET_ROLE="blue"
         export APP_VERSION="${APP_VERSION}"
         envsubst < deployment_config/deployment.yml | kubectl apply -f -
+        kubectl rollout status deployment "prayertimes-deployment-${TARGET_ROLE}"
+
         envsubst < deployment_config/service.yml | kubectl apply -f -
+
+        # list running pods
+        printf "\nLisitng available pods and their nodes"
+        kubectl get pods --output=custom-columns=Name:.metadata.name,NodeName:.spec.nodeName
+
+        printf "\n listing available services"
+        kubectl get svc -o wide
+        service_hostname=$(kubectl get svc prayertimes-service -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+        curl -I "$service_hostname"
 
     else
         if [[ "$DEPLOYED_ROLE" == "blue" ]]
@@ -24,31 +35,42 @@ main(){
         export TARGET_ROLE="blue"
 
         else
-            echo "service role was not recognized: ${DEPLOYED_ROLE}"
+            echo "Service role was not recognized: ${DEPLOYED_ROLE}"
             exit 1
         fi
 
         # list running pods
+        printf "\nLisitng available pods and their nodes"
         kubectl get pods --output=custom-columns=Name:.metadata.name,NodeName:.spec.nodeName
 
         # deploy second role
+        printf "\nDeploying role: %s" "${TARGET_ROLE}"
         envsubst < deployment_config/deployment.yml | kubectl apply -f -
 
+        printf "\nWaiting for deployment to be done"
         kubectl rollout status deployment "prayertimes-deployment-${TARGET_ROLE}"
 
-        # list running pods
-        kubectl get pods --output=custom-columns=Name:.metadata.name,NodeName:.spec.nodeName
+        printf "\nlisting existing services"
+        kubectl get svc -l app=prayertimes -o wide
+        printf "\nTesting the deployed service"
+        service_hostname=$(kubectl get svc prayertimes-service -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+        curl -I "$service_hostname"
 
+        printf "\nLisitng available pods and their nodes"
+        kubectl get pods --output=custom-columns=Name:.metadata.name,NodeName:.spec.nodeName
 
         # test with test_service
 
         # if tests successful switch service to new deployment
+        printf "\nSwitching service to new deployment"
         envsubst < deployment_config/service.yml | kubectl apply -f -
         # delete old deployment
+        printf "\nDelete old deployment"
         kubectl delete deployment "prayertimes-deployment-${DEPLOYED_ROLE}"
 
         sleep 2
         # list running pods
+        printf "\nLisitng available pods and their nodes"
         kubectl get pods --output=custom-columns=Name:.metadata.name,NodeName:.spec.nodeName
 
     fi
